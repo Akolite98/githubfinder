1d8e260baf27730fbda160f904cfca3348aeae8d
akolite <nnorompreciousakachukwu@gmail.com>
2021-02-02 00:45:59 +0100

    My github app

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '0134a22f29d873a77b79';
+    this.client_secret = '3a7e400a4b1799f7675775ce22705264558216d8';
+    this.repos_count = 6;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
